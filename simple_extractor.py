4d8f34465abd7b938720aa6f564398f7e26878e2
Saqub <saqub_ali88@hotmail.co.uk>
2023-11-20 00:30:33 +0000

    Partially_working_1st_pass

--- simple_extractor.py
+++ simple_extractor.py
@@ -1,39 +1,47 @@
 import extractor_functions
+import json
 
-db_select = '2'
+print("Welcome!\nThis script is will allow you to select the tables you wish upload to your database")
+print("The following is a list of tables available to upload to your database")
 
-answer = input("do you wish to upload the following : dim_products ?")
-if answer == 'y':
-    extractor_functions.extract_products_table()
-else:
-    pass
+with open('keys/table_key.json', 'r') as file_1:
+    table_key = json.load(file_1)
+file_1.close()
+for keys, values in table_key.items():
+    print(f"{keys} - {values}")
 
-answer = input("do you wish to upload the following : orders_table ?")
-if answer == 'y':
-    extractor_functions.extract_orders_table()
-else:
-    pass
+table_request = input("Please list tables to extract into single string :")
 
-answer = input("do you wish to upload the following : dim_store_details ?")
-if answer == 'y':
-    extractor_functions.extract_stores_table()
-else:
-    pass
+with open('keys/database_key.json', 'r') as file_2:
+    database_key = json.load(file_2)
+file_2.close()
+for keys, values in database_key.items():
+    print(f"{keys} - {values}")
 
-answer = input("do you wish to upload the following : dim_users ?")
-if answer == 'y':
-    extractor_functions.extract_users_table()
-else:
-    pass
+db_request = input("which database version would you like to upload to :")
 
-answer = input("do you wish to upload the following : dim_date_times ?")
-if answer == 'y':
-    extractor_functions.extract_date_table()
-else:
-    pass
+def filter(input_list):
+    output_set = set()
+    for x in range(0,len(input_list)):
+        if input_list[x].isnumeric() == True:
+            output_set.add((input_list[x]))
+    return list(output_set)
 
-answer = input("do you wish to upload the following : dim_card_details ?")
-if answer == 'y':
-    extractor_functions.extract_card_table(db_select)
-else:
-    pass
+requested_table_list = filter(table_request)
+requested_db_list = filter(db_request)
+print(requested_table_list)
+print(requested_db_list)
+
+if ('5' or '0') in requested_table_list:
+    extractor_functions.extract_products_table(requested_db_list)
+if ('6' or '0') in requested_table_list:
+    extractor_functions.extract_orders_table(requested_db_list)
+if ('4' or '0') in requested_table_list:
+    extractor_functions.extract_stores_table(requested_db_list)
+if ('3' or '0') in requested_table_list:
+    extractor_functions.extract_users_table(requested_db_list)
+if ('2' or '0') in requested_table_list:
+    extractor_functions.extract_date_table(requested_db_list)
+if ('1' or '0') in requested_table_list:
+    extractor_functions.extract_card_table(requested_db_list)
+else: pass
